5fe3b388853d1cd9744cf34adf4df7181114fa07
Johnny <chuaboonhock@singnet.com.sg>
2026-07-18 12:28:59 +0800

    update db.js

--- db.js
+++ db.js
@@ -1,11 +1,33 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL,
+//   ssl:
+//     process.env.NODE_ENV === "production"
+//       ? { rejectUnauthorized: false }
+//       : false,
+// });
+
+// module.exports = pool;
+
+// db.js
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Required for Neon
+  }
+});
+
+// Test the connection
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Error connecting to database:', err.stack);
+  } else {
+    console.log('Successfully connected to database');
+    release();
+  }
 });
 
 module.exports = pool;
